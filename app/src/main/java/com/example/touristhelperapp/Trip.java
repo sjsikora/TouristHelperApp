d7272f9b404f570c06977368790e7ec8647a3d3b
Sam <sikora.samj@gmail.com>
2024-12-03 10:03:32 -0800

    Added Dates

--- app/src/main/java/com/example/touristhelperapp/Trip.java
+++ app/src/main/java/com/example/touristhelperapp/Trip.java
@@ -1,14 +1,37 @@
 package com.example.touristhelperapp;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Trip {
     private String name;
+    private Date startDate;
+    private Date endDate;
     private ArrayList<Event> events;
 
     public Trip(String name) {
         this.name = name;
         this.events = new ArrayList<>();
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
     }
 
     public String getName() { return name; }
