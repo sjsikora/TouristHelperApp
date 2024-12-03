0fe3360e700c80cf391a63d179cf9f033d616ad6
Sam <sikora.samj@gmail.com>
2024-12-02 16:07:59 -0800

    Updated Trip Class

--- app/src/main/java/com/example/touristhelperapp/Trip.java
+++ app/src/main/java/com/example/touristhelperapp/Trip.java
@@ -1,9 +1,13 @@
 package com.example.touristhelperapp;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Trip {
+
     private String name;
+    private Date startDate;
+    private Date endDate;
     private ArrayList<Event> events;
 
     public Trip(String name) {
@@ -11,9 +15,32 @@ public class Trip {
         this.events = new ArrayList<>();
     }
 
+    public Trip(String tripName, String startDate, String endDate) {
+    }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public ArrayList<Event> getEvents() { return events; }
     public void addEvent(Event event) { this.events.add(event); }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
 }
