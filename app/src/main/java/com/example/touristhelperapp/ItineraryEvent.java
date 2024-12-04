fd8c943d4975433d49bc4204078ec4a82d5f73fe
Sam <sikora.samj@gmail.com>
2024-12-03 16:38:02 -0800

    Master Rebase

---
+++ app/src/main/java/com/example/touristhelperapp/ItineraryEvent.java
@@ -0,0 +1,42 @@
+package com.example.touristhelperapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItineraryEvent extends Fragment {
+
+    static final String ARG_EVENT = "event";
+
+    private Event event;
+
+    public ItineraryEvent() {}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            event = getArguments().getParcelable(ARG_EVENT, Event.class);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_itinerary_event, container, false);
+    }
+}
