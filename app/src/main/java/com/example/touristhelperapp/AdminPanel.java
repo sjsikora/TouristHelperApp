5c35f027f103486ae0f69d90224a0dce1ca127e2
Sam <sikora.samj@gmail.com>
2024-12-04 19:54:13 -0800

    More Standard Event

--- app/src/main/java/com/example/touristhelperapp/AdminPanel.java
+++ app/src/main/java/com/example/touristhelperapp/AdminPanel.java
@@ -87,9 +87,9 @@ public class AdminPanel extends BaseActivity {
         ArrayList<Event> events = new ArrayList<>();
         Calendar cal = Calendar.getInstance();
 
-        cal.set(2025, Calendar.JANUARY, 3, 8, 0);
+        cal.set(2025, Calendar.JANUARY, 3, 10, 30);
         Date time1 = cal.getTime();
-        cal.set(2025, Calendar.JANUARY, 3, 11, 0);
+        cal.set(2025, Calendar.JANUARY, 3, 11, 59);
         Date time2 = cal.getTime();
 
         events.add(new Event(
@@ -97,7 +97,7 @@ public class AdminPanel extends BaseActivity {
                 new ArrayList<String>(List.of("price", "accessibility")),
                 time1,
                 time2,
-                "Join us for an unforgettable evening under the stars! Starry Nights is a family-friendly event where you can explore the wonders of the night sky. Led by experienced astronomers, we'll guide you through constellations, planets, and other celestial objects visible that night.",
+                "Join us for an unforgettable evening under the stars! Led by experienced astronomers, we'll guide you through constellations, planets, and other celestial objects visible that night.",
                 "Okanagan Observatory, Beaverdell, BC V0H 1A0",
                 "https://science.nasa.gov/wp-content/uploads/2024/02/hubble-ngc2298-acs-wfc3-v3-5fcont-final.jpg?w=1891"
         ));
@@ -144,7 +144,37 @@ public class AdminPanel extends BaseActivity {
                 time8,
                 "Come to Mission Hill for the 3rd annual Winter wine tasting. There will be performances from local bands as well as a Christmas Play.",
                 "Mission Hill Family Estate Winery, 1730 Mission Hill Rd, West Kelowna, BC V4T 2E4",
-                "https://www.kelowna.ca/sites/files/1/styles/inside_banner/public/uploads/banners/inside/parks/stuart_park_rink.jpg?itok=DfylgVyX"
+                "https://www.missionhillwinery.com/wp-content/uploads/2020/09/MHFE_Disc_Winery_Architecture_Loggia01-780x575.jpg"
+        ));
+
+        cal.set(2025, Calendar.JANUARY, 2, 9, 0);
+        Date time9 = cal.getTime();
+        cal.set(2025, Calendar.JANUARY, 2, 13, 0);
+        Date time10 = cal.getTime();
+
+        events.add(new Event(
+                "Snowshoeing Adventure",
+                new ArrayList<String>(List.of("accessibility")),
+                time9,
+                time10,
+                "Join a guided snowshoeing trek through the pristine trails of Myra Canyon. Suitable for all fitness levels.",
+                "Myra Canyon, Kelowna, BC V1W 4G5",
+                "https://imagedelivery.net/rCY_-t_NaBnc_UkEr8yoCA/a6822616-848a-49b3-a582-f16d51c47d00/instory"
+        ));
+
+        cal.set(2025, Calendar.JANUARY, 5, 9, 30);
+        Date time11 = cal.getTime();
+        cal.set(2025, Calendar.JANUARY, 5, 13, 0);
+        Date time12 = cal.getTime();
+
+        events.add(new Event(
+                "Winter Farmers' Market",
+                new ArrayList<String>(List.of("accessibility")),
+                time11,
+                time12,
+                "Browse local produce, handmade crafts, and delicious baked goods at the Winter Farmers' Market.",
+                "Kelowna Community Centre, 1375 Water St, Kelowna, BC V1Y 1J4",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUYSW7RD9NGTV3E-wgrCwXPG4UNg5c-fyvXQ&s"
         ));
 
 
